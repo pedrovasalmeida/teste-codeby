d6990e231df48d02c9c58f8ab6f06e81bf9a24b5
Pedro Henrique <pedrovasalmeida@gmail.com>
2021-08-09 20:43:21 -0300

    feat: :sparkles: Adding Toast Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+/** Components */
+import Home from './pages/Home';
+import { ProductsProvider } from './context/ProductsContext';
+import { ToastContainer } from 'react-toastify';
+
+/** Styles */
+import GlobalStyles from './styles/global';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProductsProvider>
+      <ToastContainer />
+      <GlobalStyles />
+
+      <Home />
+      
+    </ProductsProvider>
   );
 }
 
