de688b9495606ba1fec20d7f139640b82e405574
Pedro Henrique <pedrovasalmeida@gmail.com>
2021-08-09 20:45:53 -0300

    feat: :sparkles: Creating Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/** Components */
+import { Cart } from '../../components/Cart';
+import { useProducts } from '../../context/ProductsContext';
+import { FaExchangeAlt } from 'react-icons/fa';
+
+/** Styles */
+import { Container } from './styles';
+
+const Home: React.FC = () => {
+  const { changeDatabase, databaseType } = useProducts();
+  
+  return (
+    <Container>
+        <button onClick={() => changeDatabase(databaseType === 'abaixo' ? 'acima' : 'abaixo')}><FaExchangeAlt size={14} />Mudar database</button>
+        <Cart />
+    </Container>
+  );
+}
+
+export default Home;
