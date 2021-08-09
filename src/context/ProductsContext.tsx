207189c7ed4578decfbca11c2b4d871567aa03d7
Pedro Henrique <pedrovasalmeida@gmail.com>
2021-08-09 20:45:00 -0300

    feat: :sparkles: Creating Product Context

---
+++ src/context/ProductsContext.tsx
@@ -0,0 +1,143 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+import { toast } from 'react-toastify';
+
+/** Data */
+import DatabaseAbaixo10 from '../database/abaixo-10.json';
+import DatabaseAcima10 from '../database/acima-10.json';
+
+/** Types */
+import { IProduct } from '../types/Products/IProduct';
+
+type DatabaseType = 'abaixo' | 'acima';
+
+interface CartContextProps {
+  products: IProduct[];
+  totalCartPrice: number;
+  totalItemsInCart: number;
+  databaseType: DatabaseType;
+  changeDatabase: (option: DatabaseType) => void;
+  removeProductFromList: (productId: string) => void;
+  getToast: (type: 'success' | 'error') => void;
+}
+
+const CartContext = createContext({} as CartContextProps);
+
+
+export const ProductsProvider: React.FC = ({ children }) => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [totalItemsInCart, setTotalItemsInCart] = useState(0);
+  const [totalCartPrice, setTotalCartPrice] = useState(0);
+  const [databaseType, setDatabaseType] = useState<DatabaseType>('abaixo');
+
+  function getToast(type: 'success' | 'error') {
+    toast('easy');
+  }
+
+  function getProductDataFromJSONDatabase(type: 'abaixo' | 'acima') {
+    switch(type) {
+      case 'abaixo':
+        const productListBelow10 = DatabaseAbaixo10.items.map(item => {
+          return {
+            id: item.uniqueId,
+            productId: item.productId,
+            name: item.name,
+            image: item.imageUrl,
+            priceWithDiscount: (item.price + item.priceTags[0].value) / 100,
+            price: item.price / 100,
+          }
+        });
+
+        return setProducts([...productListBelow10]);
+
+      case 'acima':
+        const productListAbove10 = DatabaseAcima10.items.map(item => {
+          return {
+            id: item.uniqueId,
+            productId: item.productId,
+            name: item.name,
+            image: item.imageUrl,
+            priceWithDiscount: (item.price + item.priceTags[0].value) / 100,
+            price: item.price / 100,
+          }
+        });
+
+        return setProducts([...productListAbove10]);
+
+      default: break;
+    }
+    
+    const productList = DatabaseAbaixo10.items.map(item => {
+      return {
+        id: item.uniqueId,
+        productId: item.productId,
+        name: item.name,
+        image: item.imageUrl,
+        priceWithDiscount: (item.price + item.priceTags[0].value) / 100,
+        price: item.price / 100,
+      }
+    });
+
+    setProducts([...productList]);
+  }
+
+  function handleWithChooseDatabase(option: DatabaseType) {
+    return setDatabaseType(option);
+  }
+
+  function removeProductFromList(productId: string) {
+    const filteredProducts = products.filter(product => product.productId !== productId);
+    
+    setProducts([...filteredProducts]);
+
+    toast.error(`Produto removido do carrinho`, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  useEffect(() => {
+    getProductDataFromJSONDatabase(databaseType);
+  }, [databaseType]);
+
+  useEffect(() => {
+    function getTotalCartPrice() {
+      let total = 0;
+
+      products.forEach(product => {
+        total += product.priceWithDiscount;
+      })
+
+      setTotalCartPrice(total);
+    }
+
+    getTotalCartPrice();
+  }, [products]);
+
+  useEffect(() => {
+    setTotalItemsInCart(products.length)
+  }, [products]);
+  
+  return (
+    <CartContext.Provider value={{ 
+      products, 
+      totalCartPrice, 
+      changeDatabase: handleWithChooseDatabase, 
+      databaseType, 
+      removeProductFromList, 
+      totalItemsInCart,
+      getToast
+    }}>{children}</CartContext.Provider>
+  );
+}
+
+export const useProducts = () => {
+  const context = useContext(CartContext);
+
+  return context;
+}
